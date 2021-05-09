c6315b2f45abd9a1a1e623b3d503da4fd891067b
m1i9n8o2r <62185304+m1i9n8o2r@users.noreply.github.com>
2021-05-09 12:40:32 -0600

    test

--- read_data.py
+++ read_data.py
@@ -13,9 +13,11 @@ readdata.close
 with open("README.txt", "r") as data:
     print(data.read())
 
+#To append a line
 with open("README.txt", "a+") as data:
     data.write("\nAgregando segunda linea por medio de with open append")
 
+#Reads the new file with the append of line 17
 with open("README.txt", "r") as data:
     print(data.read())
 
